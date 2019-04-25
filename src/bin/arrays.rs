f0ab7f238f823ad612b109b8877b2d94121fba11
Reynaldo <reynaldo.gil@hooyu.com>
2019-04-25 16:11:16 +0100

    arrays

--- src/bin/arrays.rs
+++ src/bin/arrays.rs
@@ -4,6 +4,14 @@ fn print_slice<T: Debug>(slice: &[T]) {
     println!("{:?}", slice);
 }
 
+fn sum(slice: &[u8]) -> u8 {
+    let mut sum = 0;
+    for i in slice {
+        sum += i
+    }
+    sum
+}
+
 fn main() {
     let array: [u8; 5] = [1, 2, 3, 4, 5];
 
@@ -21,6 +29,8 @@ fn main() {
 
     print!("First three elements: ");
     print_slice(&array[..3]);
+
+    println!("sum: {}", sum(&array))
 
 //print!("Oops, going too far!: ");
 //print_slice(&array[..900]);
