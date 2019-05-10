61c54c5f06471f5fc2d4122c0fe9a18ada6280de
Reynaldo <gilcu2@gmail.com>
2019-05-10 13:15:41 +0100

    temperature

---
+++ src/bin/fahrenheit_celsius.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+enum Unit {
+    Celsius,
+    Fahrenheit,
+}
+
+fn main() {
+    println!("Type the temperature with the unit");
+
+    let mut temperature = String::new();
+    io::stdin().read_line(&mut temperature).expect("Failed to read temperature");
+
+    let value = &temperature[0..temperature.len() - 2];
+    let unit = &temperature[temperature.len() - 2..];
+
+    println!("value: {} unit: {}", value, unit);
+
+    let value: i32 = value.parse().expect("temperature must be a number");
+
+    let unit = match unit {
+        "C" => Unit::Celsius,
+        "F" => Unit::Fahrenheit,
+        _ => panic!("unit must be C or F"),
+    };
+
+    match unit {
+        Unit::Celsius => println!("{}{}", value * 9 / 5 + 32, "F"),
+        Unit::Fahrenheit => println!("{}{}", (value - 32) * 5 / 9, "C"),
+    }
+}
