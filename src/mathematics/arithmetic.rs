0b80edd99e7c37fc02eb64ae280b6650af601d13
reynaldo.gil <reynaldo.gil@hooyu.com>
2019-05-16 07:39:52 +0100

    unit test module

--- src/mathematics/arithmetic.rs
+++ src/mathematics/arithmetic.rs
@@ -1,2 +1,13 @@
-pub fn sum(a: i8, b: i8) -> i8 { a + b }
+pub fn sum(a: i8, b: i8) -> i8 { private_sum(a, b) }
 
+fn private_sum(a: i8, b: i8) -> i8 { a + b }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_integration() {
+        assert_eq!(private_sum(6, 8), 14);
+    }
+}
