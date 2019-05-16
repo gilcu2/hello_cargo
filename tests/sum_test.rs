b54b1ed2e34d31ec88265d0be7b5da2935ae701e
reynaldo.gil <reynaldo.gil@hooyu.com>
2019-05-16 07:20:19 +0100

    test module

--- tests/sum_test.rs
+++ tests/sum_test.rs
@@ -2,7 +2,12 @@ extern crate hello_cargo;
 
 use hello_cargo::arithmetic;
 
-#[test]
-fn test_sum_integration() {
-    assert_eq!(arithmetic::sum(6, 8), 14);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sum_integration() {
+        assert_eq!(arithmetic::sum(6, 8), 14);
+    }
 }
