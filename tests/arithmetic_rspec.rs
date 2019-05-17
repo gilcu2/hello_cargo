e350210ed9fde8128f8a5341a75a07c7a07e6bb7
Reynaldo <gilcu2@gmail.com>
2019-05-17 08:44:18 +0100

    comment test

--- tests/arithmetic_rspec.rs
+++ tests/arithmetic_rspec.rs
@@ -5,20 +5,20 @@ use std::io;
 use std::sync::Arc;
 
 
-#[test]
-pub fn main() {
-    let logger = Arc::new(rspec::Logger::new(io::stdout()));
-    let configuration = rspec::ConfigurationBuilder::default().build().unwrap();
-    let runner = rspec::Runner::new(configuration, vec![logger]);
-
-    runner.run(
-        &rspec::given("an value of ten", 10, |ctx| {
-            ctx.when("adding 5 to it", |ctx| {
-                ctx.before(|value| { *value + 5 });
-
-                ctx.then("results in fifteen", |num| {
-                    assert_eq!(*num, 15);
-                });
-            });
-        }));
-}
+//#[test]
+//pub fn main() {
+//    let logger = Arc::new(rspec::Logger::new(io::stdout()));
+//    let configuration = rspec::ConfigurationBuilder::default().build().unwrap();
+//    let runner = rspec::Runner::new(configuration, vec![logger]);
+//
+//    runner.run(
+//        &rspec::given("an value of ten", 10, |ctx| {
+//            ctx.when("adding 5 to it", |ctx| {
+//                ctx.before(|value| { *value + 5 });
+//
+//                ctx.then("results in fifteen", |num| {
+//                    assert_eq!(*num, 15);
+//                });
+//            });
+//        }));
+//}
